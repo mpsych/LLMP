5ee2b59d5daef20f19075927145ddeca12d83738
mahsa <mahsa.geshvadi001@umb.edu>
2024-01-25 13:38:29 -0500

    new questions

--- LLMP/chatgpt.py
+++ LLMP/chatgpt.py
@@ -23,7 +23,7 @@ class ChatGPT:
         base64_image = base64.b64encode(png_bytes).decode('utf-8')
 
         # OpenAI API Key
-        api_key = "*"
+        api_key = "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"
 
         headers = {
             "Content-Type": "application/json",
